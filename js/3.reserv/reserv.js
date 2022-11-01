3f5466a017c31a10f26cdbeb051e190f71beaf21
Inqui012 <xii0712@gmail.com>
2022-11-01 16:42:55 +0900

    .

--- js/3.reserv/reserv.js
+++ js/3.reserv/reserv.js
@@ -29,17 +29,16 @@ $(".btn-01").click(function () {
 // 완료버튼 누르면서 변수저장
 $(".btn-02").click(function (e) {
     e.preventDefault();
-    nameCheck();
-    telCheck();
-    // visitCheck();
-    documentCheck();
-    console.log($appointDocu);
-
-    $appointDate = "2022. " + $monthVal + ". " + $("#select-day option:selected").val();
-    $appointName = $("#user-name").val();
-    $appointTel = $("#user-tel").val();
-    $appoint_patient = [1, $appointName, '이비인후과', '김범주', '1 진료실', $appointDate, '14:30', '예약 완료', $appointDocu];
-    sessionStorage.setItem("appoint", JSON.stringify($appoint_patient));
+    if (nameCheck() && telCheck()) {
+        $("main div.reserv-complete").css({ display: "block" });
+        documentCheck();
+        $appointDate = "2022. " + $monthVal + ". " + $("#select-day option:selected").val();
+        $appointName = $("#user-name").val();
+        $appointTel = $("#user-tel").val();
+        $appoint_patient = [1, $appointName, '이비인후과', '김범주', '1 진료실', $appointDate, '14:30', '예약 완료', $appointDocu];
+        sessionStorage.setItem("appoint", JSON.stringify($appoint_patient));
+        reservCompCount();
+    }
 })
 
 // 펑션구역
