97ab059f684de60eaa61c616f2ca1c113ed64c78
MolRue <mctothej@gmail.com>
2022-11-03 16:19:14 +0900

    index done

--- js/1.index/index1.js
+++ js/1.index/index1.js
@@ -28,22 +28,62 @@ var swiper = new Swiper('.swiper', {
   },
 });
 
-// $(function () {
-//   $(function () {
-//     $('.icon').click(function () {
-//       $('.icon').parent().next().css({ color: '#324521' });
-//     });
+const contents = document.querySelectorAll('.reservation-box1');
+const button = document.querySelectorAll('.reservation-box1');
 
-//     $('.icon').on({
-//       'mouseover focus': function () {
-//         $('.icon').parent().next().css({ color: 'red' });
-//       },
-//       'mouseout blur': function () {
-//         $('.icon').parent().next().css({ color: 'black' });
-//       },
-//     });
-//     $('.icon').off('click');
-//     $('.icon').off('mouseover');
-//     // .off : 이벤트를 제거한다. 보통은 특정 경우에 제거되도록 설정한다.
-//   });
-// });
+for (let i = 0; i < button.length; i++) {
+  button[i].addEventListener('mouseover', function () {
+    contents[i].classList.add('opacity40');
+  });
+}
+
+for (let i = 0; i < button.length; i++) {
+  button[i].addEventListener('mouseout', function () {
+    contents[i].classList.remove('opacity40');
+  });
+}
+
+const contents2 = document.querySelectorAll('.reservation-box2');
+const button2 = document.querySelectorAll('.reservation-box2');
+
+for (let i = 0; i < button.length; i++) {
+  button2[i].addEventListener('mouseover', function () {
+    contents2[i].classList.add('opacity40');
+  });
+}
+
+for (let i = 0; i < button.length; i++) {
+  button2[i].addEventListener('mouseout', function () {
+    contents2[i].classList.remove('opacity40');
+  });
+}
+
+const contents3 = document.querySelectorAll('.reservation-box3');
+const button3 = document.querySelectorAll('.reservation-box3');
+
+for (let i = 0; i < button.length; i++) {
+  button3[i].addEventListener('mouseover', function () {
+    contents3[i].classList.add('opacity40');
+  });
+}
+
+for (let i = 0; i < button.length; i++) {
+  button3[i].addEventListener('mouseout', function () {
+    contents3[i].classList.remove('opacity40');
+  });
+}
+
+const contents4 = document.querySelectorAll('.reservation-box4');
+const button4 = document.querySelectorAll('.reservation-box4');
+
+for (let i = 0; i < button.length; i++) {
+  button4[i].addEventListener('mouseover', function () {
+    contents4[i].classList.add('opacity40');
+  });
+}
+
+for (let i = 0; i < button.length; i++) {
+  button4[i].addEventListener('mouseout', function () {
+    contents4[i].classList.remove('opacity40');
+  });
+}
